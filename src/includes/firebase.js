22480ad68a3a023f2ab87826970cbffd27dbe037
Abdumalik2022 <97823118+Abdumalik2022@users.noreply.github.com>
2022-09-20 05:02:39 +0500

    progress bar will add

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 
 // Web app's Firebase configuration
 const firebaseConfig = {
@@ -17,7 +18,8 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection("users");
 
-export { auth, usersCollection };
+export { auth, usersCollection, storage };
